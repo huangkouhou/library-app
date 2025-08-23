3022ed15d55753610c43f845bcf27381db2363c3
Peng Huang <huangkouhou@gmail.com>
2025-08-23 14:28:04 +0900

    React Router - Switch and Redirect Routing

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import './App.css';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-import { Footer } from './layouts/NavbarAndFooter/Footer';
-import { HomePage } from './layouts/HomePage/HomePage';
-import { SearchBooksPage } from './layouts/SearchBookPage/SearchBooksPage';
+import "./App.css";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { SearchBooksPage } from "./layouts/SearchBookPage/SearchBooksPage";
+import { Redirect, Route, Switch } from "react-router-dom"; //v5，Switch 就是“只渲染第一个匹配 Route”的开关；如果是 v6，用 Routes，别再用 Switch。
 
 export const App = () => {
   return (
     <div>
       <Navbar />
-        {/* <HomePage /> */}
-      <SearchBooksPage />
+      <Switch>
+        <Route path="/" exact> {/*Switch 常配合 exact，避免 / 抢先匹配更长路径。*/}
+          <Redirect to='/home' />
+        </Route>
+        <Route path='/home'>
+          <HomePage />
+        </Route>
+        <Route path="/search">
+          <SearchBooksPage />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
-}
-
-
+};
--- 03-frontend/react-library/src/index.tsx
+++ 03-frontend/react-library/src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
+import { BrowserRouter } from 'react-router-dom';//顶层路由容器,一般全站只放 1 个，包住应用。没有它，Routes/Route、Link/NavLink、useNavigate 等都不能用。
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 
