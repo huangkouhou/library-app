fb34307296547a27ef1f0f2189f887cae5219a63
Peng Huang <huangkouhou@gmail.com>
2025-08-24 11:27:08 +0900

    Book Checkout Page

--- 03-frontend/react-library/src/App.tsx
+++ 03-frontend/react-library/src/App.tsx
@@ -4,16 +4,15 @@ import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { HomePage } from "./layouts/HomePage/HomePage";
 import { SearchBooksPage } from "./layouts/SearchBookPage/SearchBooksPage";
 import { Redirect, Route, Switch } from "react-router-dom"; //v5，Switch 就是“只渲染第一个匹配 Route”的开关；如果是 v6，用 Routes，别再用 Switch。
+import { BookCheckoutPage } from "./layouts/BookCheckoutPage/BookCheckoutPage";
 
 export const App = () => {
   return (
-    <div className="d-flex flex-column min-vh-100">
+    <div className="d-flex flex-column min-vh-100"> {/*容器最小高度占一整屏。内容不够高时也撑满屏幕*/}
       <Navbar />
-      <div className="flex-grow-1">
+      <div className="flex-grow-1"> {/*让中间这块主内容占据剩余空间，等价于把 Footer 推到底部*/}
         <Switch>
-          <Route path="/" exact>
-            {" "}
-            {/*Switch 常配合 exact，避免 / 抢先匹配更长路径。*/}
+          <Route path="/" exact> {/*Switch 常配合 exact，避免 / 抢先匹配更长路径。*/}
             <Redirect to="/home" />
           </Route>
           <Route path="/home">
@@ -21,6 +20,9 @@ export const App = () => {
           </Route>
           <Route path="/search">
             <SearchBooksPage />
+          </Route>
+          <Route path='/checkout/:bookId'>
+            <BookCheckoutPage />
           </Route>
         </Switch>
       </div>
---
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+
+export const BookCheckoutPage = () => {
+  // ① 在组件顶层定义状态
+  const [book, setBook] = useState<BookModel>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  const bookId = window.location.pathname.split("/")[2]; //for example localhost:3000/checkout/<bookId>
+
+  // ② 在 useEffect 里，根据“依赖数组”去更新状态
+  useEffect(() => {
+    //useEffect(() => { ... }, ...) 的回调函数，写副作用逻辑（取数、订阅、定时器等）。
+    const fetchBook = async () => {
+      const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+
+      const response = await fetch(baseUrl);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseJson = await response.json();
+
+      const loadedBook: BookModel = {
+        id: responseJson.id,
+        title: responseJson.title,
+        author: responseJson.author,
+        description: responseJson.description,
+        copies: responseJson.copies,
+        copiesAvailable: responseJson.copiesAvailable,
+        category: responseJson.category,
+        img: responseJson.img,
+      };
+
+      setBook(loadedBook);
+      setIsLoading(false);
+    };
+    fetchBook().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []); //[...] 依赖数组，决定何时运行：只在首次挂载运行一次；[a, b]：当 a 或 b 变化时运行。省略 []：每次渲染后都运行（少用）。
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="container d-none d-lg-block">
+        <div className="row mt-5">
+          <div className="col-sm-2 col-md-2">
+            {book?.img ? (
+              <img src={book?.img} width="226" height="349" alt="Book" />
+            ) : (
+              <img
+                src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                width="226"
+                height="349"
+                alt="Book"
+              />
+            )}
+          </div>
+          <div className="col-4 col-md-4 container">
+            <div className="ml-2">
+              <h2>{book?.title}</h2>
+              <h5 className="text-primary">{book?.author}</h5>
+              <p className="lead">{book?.description}</p>
+            </div>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <div className="container d-lg-none mt-5">
+        <div className="d-flex justify-content-center align-items-center">
+          {book?.img ? (
+            <img src={book?.img} width="226" height="349" alt="Book" />
+          ) : (
+            <img
+              src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+              width="226"
+              height="349"
+              alt="Book"
+            />
+          )}
+        </div>
+        <div className="mt-4">
+          <div className="ml-2">
+            <h2>{book?.title}</h2>
+            <h5 className="text-primary">{book?.author}</h5>
+            <p className="lead">{book?.description}</p>
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
