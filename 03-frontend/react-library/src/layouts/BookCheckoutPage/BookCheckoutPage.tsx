334bcb43c380a933f9271dbb9bfa9d99e79cccf8
Peng Huang <huangkouhou@gmail.com>
2025-08-25 14:58:11 +0900

    Review useEffect

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -3,12 +3,18 @@ import BookModel from "../../models/BookModel";
 import { SpinnerLoading } from "../Utils/SpinnerLoading";
 import { StarsReview } from "../Utils/StarsReview";
 import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
+import ReviewModel from "../../models/ReviewModel";
 
 export const BookCheckoutPage = () => {
   // ① 在组件顶层定义状态
   const [book, setBook] = useState<BookModel>();
   const [isLoading, setIsLoading] = useState(true);
   const [httpError, setHttpError] = useState(null);
+
+  //Review State
+  const [reviews, setReviews] = useState<ReviewModel[]>([]);
+  const [totalStars, setTotalStars] = useState(0);
+  const [isLoadingReview, setIsLoadingReview] = useState(true);
 
   const bookId = window.location.pathname.split("/")[2]; //for example localhost:3000/checkout/<bookId>
 
@@ -46,7 +52,57 @@ export const BookCheckoutPage = () => {
     });
   }, []); //[...] 依赖数组，决定何时运行：只在首次挂载运行一次；[a, b]：当 a 或 b 变化时运行。省略 []：每次渲染后都运行（少用）。
 
-  if (isLoading) {
+
+  //Review useEffect
+  useEffect(() => {
+    const fetchBookReviews = async () => {
+      const reviewUrl: string = `http://localhost:8080/api/reviews/search/findByBookId?bookId=${bookId}`;
+
+      const responseReviews = await fetch(reviewUrl);
+
+      if (!responseReviews.ok){
+          throw new Error('Something went wrong!');
+      }
+
+      const responseJsonReviews = await responseReviews.json();
+
+      const responseData = responseJsonReviews._embedded.reviews;
+
+      const loadedReviews: ReviewModel[] = [];
+
+      let weightedStarReviews: number = 0;
+
+      for (const key in responseData) {
+        loadedReviews.push({
+          id: responseData[key].id,
+          userEmail: responseData[key].userEmail,
+          date: responseData[key].date,
+          rating: responseData[key].rating,
+          book_id: responseData[key].bookId,
+          reviewDescription: responseData[key].reviewDescription,
+        });
+        weightedStarReviews = weightedStarReviews + responseData[key].rating;//把所有评论的评分相加review的“总分”
+      }
+      
+      //把“所有评分的平均值”四舍五入到最接近的 0.5，并存进状态 totalStars
+      //* 2 再 Math.round(...) 再 / 2：把平均分按 0.5 为步长取整。
+      //.toFixed(1)：把数字格式化成 1 位小数的字符串（比如 "4.5"）。
+      if (loadedReviews){
+        const round = (Math.round((weightedStarReviews / loadedReviews.length) * 2) / 2).toFixed(1);
+        setTotalStars(Number(round));
+      }
+
+      setReviews(loadedReviews);
+      setIsLoadingReview(false);
+    };
+
+    fetchBookReviews().catch((error: any) => {
+      setIsLoadingReview(false);
+      setHttpError(error.message);
+    })
+  }, []);
+
+  if (isLoading || isLoadingReview) {
     return <SpinnerLoading />;
   }
 
