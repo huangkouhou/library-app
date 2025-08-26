e82413fd871ce934a105f457afb4f79992b8bbe6
Peng Huang <huangkouhou@gmail.com>
2025-08-26 14:42:16 +0900

    Add Login and Logout Function to Navbar

--- 03-frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
+++ 03-frontend/react-library/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,6 +1,41 @@
 import { NavLink } from "react-router-dom";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { useAuth0 } from "@auth0/auth0-react";
+import React, { useEffect, useState } from "react";
 
 export const Navbar = () => {
+  const [roles, setRoles] = useState<string[] | null>(null);
+  const [loading, setLoading] = useState(true); // Loading state to handle async data
+  const { isAuthenticated, loginWithRedirect, logout, getIdTokenClaims } =
+    useAuth0();
+
+  useEffect(() => {
+    const fetchRoles = async () => {
+      const claims = await getIdTokenClaims();
+      const fetchedRoles =
+        claims?.["https://luv2code-react-library.com/roles"] || [];
+      setRoles(fetchedRoles);
+      setLoading(false); // Set loading to false once roles are loaded
+    };
+
+    fetchRoles();
+  }, [isAuthenticated, getIdTokenClaims]);
+
+  if (loading) {
+    return <SpinnerLoading />;
+  }
+
+  const handleLogout = () => {
+    console.log("handleLogout");
+    logout({ logoutParams: { returnTo: window.location.origin } });
+  };
+
+  const handleLogin = () => {
+    loginWithRedirect(); //调用 Auth0 的登录：跳到 Auth0 登录页
+    window.location.assign("/"); //立刻把浏览器重定向到站点根路径（整页刷新）
+  };
+
+  console.log("isAuthenticated: ", isAuthenticated);
   return (
     <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
       <div className="container-fluid">
@@ -28,13 +63,40 @@ export const Navbar = () => {
                 Search Books
               </NavLink>
             </li>
+
+            {isAuthenticated && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/shelf">
+                  Shelf
+                </NavLink>
+              </li>
+            )}
+
+            {isAuthenticated && roles?.includes("admin") && (
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/admin">
+                  Admin
+                </NavLink>
+              </li>
+            )}
           </ul>
           <ul className="navbar-nav ms-auto">
-            <li className="nav-item m-1">
-              <a type="button" className="btn btn-outline-light" href="#">
-                Sign in
-              </a>
-            </li>
+            {!isAuthenticated ? (
+              <li className="nav-item m-1">
+                <button className="btn btn-outline-light" onClick={handleLogin}>
+                  Sign in
+                </button>
+              </li>
+            ) : (
+              <li>
+                <button
+                  className="btn btn-outline-light"
+                  onClick={handleLogout}
+                >
+                  Logout
+                </button>
+              </li>
+            )}
           </ul>
         </div>
       </div>
