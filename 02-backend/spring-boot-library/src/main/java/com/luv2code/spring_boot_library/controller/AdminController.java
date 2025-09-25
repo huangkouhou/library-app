72e238a052e42e052a693bd5f0012c517629cc25
Peng Huang <huangkouhou@gmail.com>
2025-09-25 10:41:56 +0900

    Backend Increase Book Quantity Controller

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/controller/AdminController.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/controller/AdminController.java
@@ -7,8 +7,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -26,6 +28,18 @@ public class AdminController {
 
     public AdminController(AdminService adminService){
         this.adminService = adminService;
+    }
+
+    @PutMapping("/increase/book/quantity")
+    public void increaseBookQuantity(@AuthenticationPrincipal Jwt jwt,
+                                     @RequestParam Long bookId) throws Exception{
+    
+    List<String> roles = jwt.getClaimAsStringList(ROLES_CLAIM);
+    boolean isAdmin = roles != null && roles.stream().anyMatch(r -> "admin".equalsIgnoreCase(r));
+    if (!isAdmin) {
+        throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Administration page only.");
+    }
+        adminService.increaseBookQuantity(bookId);                       
     }
 
     @PostMapping("/add/book")
