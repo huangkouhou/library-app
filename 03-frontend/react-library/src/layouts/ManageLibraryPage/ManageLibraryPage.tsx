8ed5eb629c0ab99ccf579d1064f3851705bc1164
Peng Huang <huangkouhou@gmail.com>
2025-09-30 10:22:34 +0900

    update ManageLibraryPage

--- 03-frontend/react-library/src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ 03-frontend/react-library/src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -1,85 +1,129 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import { useEffect, useState } from 'react';
-import { AdminMessages } from './components/AdminMessages';
-import { AddNewBook } from './components/AddNewBook';
-import { ChangeQuantityOfBooks } from './components/ChangeQuantityOfBooks';
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
+import { AdminMessages } from "./components/AdminMessages";
+import { AddNewBook } from "./components/AddNewBook";
+import { ChangeQuantityOfBooks } from "./components/ChangeQuantityOfBooks";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { Redirect } from "react-router";
 
 export const ManageLibraryPage = () => {
+  const { getIdTokenClaims } = useAuth0(); //ID Token 是给前端用来识别用户的。
+  const [roles, setRoles] = useState<string[] | null>(null); // set roles to null initially
+  const [loading, setLoading] = useState(true); // Loading state to handle async data
 
-    const { getIdTokenClaims } = useAuth0();//ID Token 是给前端用来识别用户的。
-    const [roles, setRoles] = useState<string[] | null>(null);// set roles to null initially
-    const [loading, setLoading] = useState(true); // Loading state to handle async data
+  const [changeQuantityOfBooksClick, setChangeQuantityOfBooksClick] =
+    useState(false);
+  const [messagesClick, setMessagesClick] = useState(false);
 
-    const [changeQuantityOfBooksClick, setChangeQuantityOfBooksClick] = useState(false);
-    const [messagesClick, setMessagesClick] = useState(false);
+  useEffect(() => {
+    const fetchRoles = async () => {
+    const claims = await getIdTokenClaims();
+    const fetchedRoles = claims?.["https://library-app.local/roles"] ?? [];
+    setRoles(fetchedRoles);
+    setLoading(false);//set loading to false once roles are loaded
+    };
+    fetchRoles();
+  },[getIdTokenClaims]);
 
+  function addBookClickFunction() {
+    setChangeQuantityOfBooksClick(false);
+    setMessagesClick(false);
+  }
 
-    useEffect(() => {
-        const fetchRoles = async () => {
+  function changeQuantityOfBooksClickFunction() {
+    setChangeQuantityOfBooksClick(true);
+    setMessagesClick(false);
+  }
 
-        };
-    })
+  function messagesClickFunction() {
+    setChangeQuantityOfBooksClick(false);
+    setMessagesClick(true);
+  }
 
-    function addBookClickFunction(){
-        setChangeQuantityOfBooksClick(false);
-        setMessagesClick(false);
-    }
+  if (loading){
+    return (<SpinnerLoading/>)
+  }
 
-    function changeQuantityOfBooksClickFunction(){
-        setChangeQuantityOfBooksClick(true);
-        setMessagesClick(false);
-    }
+  if (!roles?.includes('admin')){
+    return <Redirect to='/home'/>
+  }
 
-    function messagesClickFunction(){
-        setChangeQuantityOfBooksClick(false);
-        setMessagesClick(true);
-    }
-    
-
-    return (
-        <div className='container'>
-            <div className='mt-5'>
-                <h3>Manage Library</h3>
-                <nav>
-                    <div className='nav nav-tabs' id='nav-tab' role='tablist'>
-                        <button onClick={addBookClickFunction} className='nav-link active' id='nav-add-book-tab' data-bs-toggle='tab'
-                            data-bs-target='#nav-add-book' type='button' role='tab' aria-controls='nav-add-book'
-                            aria-selected='false'
-                        >
-                            Add new book
-                        </button>
-                        <button onClick={changeQuantityOfBooksClickFunction} className='nav-link' id='nav-quantity-tab' data-bs-toggle='tab'
-                            data-bs-target='#nav-quantity' type='button' role='tab' aria-controls='nav-add-book'
-                            aria-selected='true'
-                        >
-                            Change quantity
-                        </button>
-                        <button onClick={messagesClickFunction} className='nav-link' id='nav-messages-tab' data-bs-toggle='tab'
-                            data-bs-target='#nav-messages' type='button' role='tab' aria-controls='nav-messages'
-                            aria-selected='false'
-                        >
-                            Messages
-                        </button>
-                    </div>
-                </nav>
-                <div className='tab-content' id='nav-tabContent'>
-                    <div className='tab-pane fade show active' id='nav-add-book' role='tabpanel'
-                        aria-labelledby='nav-add-book-tab'>
-                        <AddNewBook />
-                    </div>
-                    <div className='tab-pane fade' id='nav-quantity' role='tabpanel'
-                        aria-labelledby='nav-quantity-tab'>
-                        {changeQuantityOfBooksClick? <ChangeQuantityOfBooks/> : <></>}
-                    </div>
-                    <div className='tab-pane fade' id='nav-messages' role='tabpanel'
-                        aria-labelledby='nav-messages-tab'>
-                        {messagesClick ? <AdminMessages/> : <></>}
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="mt-5">
+        <h3>Manage Library</h3>
+        <nav>
+          <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <button
+              onClick={addBookClickFunction}
+              className="nav-link active"
+              id="nav-add-book-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-add-book"
+              type="button"
+              role="tab"
+              aria-controls="nav-add-book"
+              aria-selected="false"
+            >
+              Add new book
+            </button>
+            <button
+              onClick={changeQuantityOfBooksClickFunction}
+              className="nav-link"
+              id="nav-quantity-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-quantity"
+              type="button"
+              role="tab"
+              aria-controls="nav-add-book"
+              aria-selected="true"
+            >
+              Change quantity
+            </button>
+            <button
+              onClick={messagesClickFunction}
+              className="nav-link"
+              id="nav-messages-tab"
+              data-bs-toggle="tab"
+              data-bs-target="#nav-messages"
+              type="button"
+              role="tab"
+              aria-controls="nav-messages"
+              aria-selected="false"
+            >
+              Messages
+            </button>
+          </div>
+        </nav>
+        <div className="tab-content" id="nav-tabContent">
+          <div
+            className="tab-pane fade show active"
+            id="nav-add-book"
+            role="tabpanel"
+            aria-labelledby="nav-add-book-tab"
+          >
+            <AddNewBook />
+          </div>
+          <div
+            className="tab-pane fade"
+            id="nav-quantity"
+            role="tabpanel"
+            aria-labelledby="nav-quantity-tab"
+          >
+            {changeQuantityOfBooksClick ? <ChangeQuantityOfBooks /> : <></>}
+          </div>
+          <div
+            className="tab-pane fade"
+            id="nav-messages"
+            role="tabpanel"
+            aria-labelledby="nav-messages-tab"
+          >
+            {messagesClick ? <AdminMessages /> : <></>}
+          </div>
         </div>
-    );
-}
-
+      </div>
+    </div>
+  );
+};
 
 //const NS = 'https://library-app.local';
