a80683fdcd067c76e8e4f2d4bfe639b783c2baa7
Peng Huang <huangkouhou@gmail.com>
2025-10-01 10:42:23 +0900

    Stripe Setting and Spring Boot Payment Entity

---
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/entity/Payment.java
@@ -0,0 +1,27 @@
+package com.luv2code.spring_boot_library.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "payment")
+@Data
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "amount")
+    private double amount;
+
+}
