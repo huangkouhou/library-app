ee1c6038ea8775b7d4c127f5ac82941c6f6e2bcd
Peng Huang <huangkouhou@gmail.com>
2025-10-02 10:49:31 +0900

    Backend Payment Service

---
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/service/PaymentService.java
@@ -0,0 +1,57 @@
+package com.luv2code.spring_boot_library.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luv2code.spring_boot_library.dao.PaymentRepository;
+import com.luv2code.spring_boot_library.entity.Payment;
+import com.luv2code.spring_boot_library.requestmodels.PaymentInfoRequest;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Service
+@Transactional
+public class PaymentService {
+
+    private PaymentRepository paymentRepository;
+
+    public PaymentService(PaymentRepository paymentRepository, @Value("${stripe.key.secret}") String secretKey) {
+        this.paymentRepository = paymentRepository;
+        Stripe.apiKey = secretKey;
+    }
+
+    public PaymentIntent createPaymentIntent(PaymentInfoRequest paymentInfoRequest) throws StripeException {
+        List<String> paymentMethodTypes = new ArrayList<>();
+        paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfoRequest.getAmount());
+        params.put("currency", paymentInfoRequest.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
+    }
+
+    public ResponseEntity<String> stripePayment(String userEmail) throws Exception {
+        Payment payment = paymentRepository.findByUserEmail(userEmail);
+
+        if (payment == null){
+            throw new Exception("payment information is missing");
+        }
+        payment.setAmount(00.00); //将该用户记录中的待支付金额重置为 0
+        paymentRepository.save(payment); //将更新后的 Payment 对象保存回数据库
+        return new ResponseEntity<>(HttpStatus.OK);//向调用者（Controller 层）返回一个表示“操作成功”的 HTTP 200 OK 响应。
+
+    }
+
+}
