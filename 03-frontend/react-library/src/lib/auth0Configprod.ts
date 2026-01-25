9ecdd7a40523e3d659b3cb03be2a250f40b6cb7d
Peng Huang <huangkouhou@gmail.com>
2026-01-25 16:32:52 +0900

    fix(auth) config

--- 03-frontend/react-library/src/lib/auth0Configprod.ts
+++ 03-frontend/react-library/src/lib/auth0Configprod.ts
@@ -1,5 +1,5 @@
 export const auth0Config = {
- clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
+ clientId: process.env.REACT_APP_AUTH0_CLIENT_ID!,
  issuer: 'dev-tijrtha6cwckmy1o.us.auth0.com',
  audience: "https://library.penghuang.dev",
  redirectUri: "https://library.penghuang.dev/callback",
