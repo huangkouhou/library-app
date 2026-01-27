b15d43a2f2b7202bf57c1bf06bb8383fbc39b01d
Peng Huang <huangkouhou@gmail.com>
2026-01-27 16:38:13 +0900

    fix: use unique tag 'fixed' to bypass docker cache

--- 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/SpringBootLibraryApplication.java
+++ 02-backend/spring-boot-library/src/main/java/com/luv2code/spring_boot_library/SpringBootLibraryApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootLibraryApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println(">>> V3.0: FINAL FIX DEPLOYED <<<");
+
 		SpringApplication.run(SpringBootLibraryApplication.class, args);
 	}
 
